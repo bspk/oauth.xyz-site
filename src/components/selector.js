05c31586fc2d15cfb75304d26c81438fca886668
Justin Richer <justin@bspk.io>
2019-04-17 12:11:34 -0400

    rounded selectors

--- src/components/selector.js
+++ src/components/selector.js
@@ -9,9 +9,9 @@ const Selector = ({onChange, label, selected, all}) => {
   return (
     <ToggleButtonGroup name={label} value={all ? "" : selected} onChange={onChange} size="sm">
       <ToggleButton variant="link" disabled value="label">{label}: </ToggleButton>
-      <ToggleButton variant={all ? "warning" : "primary"} value="full">Full</ToggleButton>
+      <ToggleButton variant={all ? "warning" : "primary"} style={{borderTopLeftRadius: "40%", borderBottomLeftRadius: "40%"}} value="full">Full</ToggleButton>
       <ToggleButton variant={all ? "warning" : "primary"} value="handle">Handle</ToggleButton>
-      <ToggleButton variant={all ? "warning" : "primary"} value="omit">Omit</ToggleButton>
+      <ToggleButton variant={all ? "warning" : "primary"} style={{borderTopRightRadius: "40%", borderBottomRightRadius: "40%"}} value="omit">Omit</ToggleButton>
     </ToggleButtonGroup>
   );
 }
