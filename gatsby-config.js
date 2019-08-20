9d9e283dd5f131706ded40ae037727c1f4acb6fd
Justin Richer <justin@bspk.io>
2019-08-20 18:01:43 -0400

    update icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
         background_color: '#883344',
         theme_color: '#883344',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/xyz.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
